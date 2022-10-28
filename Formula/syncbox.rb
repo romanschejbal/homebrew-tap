d948097aaf924affa3af78a03a03421a7fd93ecb
Roman Schejbal <schejbalroman@gmail.com>
2022-10-28 16:21:27 +0200

    Release syncbox v0.2.0

--- Formula/syncbox.rb
+++ Formula/syncbox.rb
@@ -1,8 +1,8 @@
 class Syncbox < Formula
     desc ""
     homepage ""
-    url "https://github.com/romanschejbal/syncbox/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "669855c91fb4f33bea11ec986b840579bf523ed7390f805654538c5cd2265307"
+    url "https://github.com/romanschejbal/syncbox/archive/refs/tags/v0.2.0.tar.gz"
+    sha256 "af4d6ed69d725bd7f98af29c2bfc52ec4be5373f3628c8e4cfa2eb6e155ed006"
     license "MIT"
 
     depends_on "rust" => :build
