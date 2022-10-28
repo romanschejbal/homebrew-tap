e68e1f5f1afbd954da333b38e1d2943d23bba1bc
Roman Schejbal <schejbalroman@gmail.com>
2022-10-28 13:18:04 +0200

    Fix sha

--- Formula/syncbox.rb
+++ Formula/syncbox.rb
@@ -2,7 +2,7 @@ class Syncbox < Formula
     desc ""
     homepage ""
     url "https://github.com/romanschejbal/syncbox/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "f85279e9dcd37d0a5fa15c8d6d5e6a7ef964c38bb69da65798539b3c0e0ad605"
+    sha256 "669855c91fb4f33bea11ec986b840579bf523ed7390f805654538c5cd2265307"
     license "MIT"
 
     depends_on "rust" => :build
