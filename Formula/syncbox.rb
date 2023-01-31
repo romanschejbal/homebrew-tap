907f48171b5e83d4219e2d4033a23328f03ae869
Roman Schejbal <schejbalroman@gmail.com>
2023-01-31 13:48:35 +0100

    Update syncbox.rb

--- Formula/syncbox.rb
+++ Formula/syncbox.rb
@@ -1,8 +1,8 @@
 class Syncbox < Formula
     desc ""
     homepage ""
-    url "https://github.com/romanschejbal/syncbox/archive/refs/tags/v0.3.0.tar.gz"
-    sha256 "37e72ba562eb03103f4e05fc7d6dd92bd7385cc6e005752d5d192a32a6976e6c"
+    url "https://github.com/romanschejbal/syncbox/archive/refs/tags/v0.3.1.tar.gz"
+    sha256 "8ba7272291c86330f30865bf826e27e78e008634eb27eea617521dc40db4c4db"
     license "MIT"
 
     depends_on "rust" => :build
