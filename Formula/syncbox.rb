2ca88cb8ad4e6336d238774ee475370aad31e0e2
Roman Schejbal <schejbalroman@gmail.com>
2024-05-15 12:27:05 +1200

    syncbox v0.5.1

--- Formula/syncbox.rb
+++ Formula/syncbox.rb
@@ -1,8 +1,8 @@
 class Syncbox < Formula
     desc "Sync files and directories to various endpoints."
     homepage "https://github.com/romanschejbal/syncbox"
-    url "https://github.com/romanschejbal/syncbox/releases/download/v0.5.0/aarch64-apple-darwin.zip"
-    sha256 "2bfc7690932ae0da636f5ae7cc9fd2574fa35e7f5b5aa9cada9174459a612acd"
+    url "https://github.com/romanschejbal/syncbox/releases/download/v0.5.1/aarch64-apple-darwin.zip"
+    sha256 "946a190a0548a7266f488f2604c08e2e53705a15820565247cad189dd6d50c08"
     license "MIT"
 
     def install
